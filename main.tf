0be0b37ba6ac5480ee4473b7167da820ac1754d0
saikrishnap123 <perabathulasaimanikanta@gmail.com>
2022-07-18 13:27:27 +0530

    featuere

--- main.tf
+++ main.tf
@@ -75,7 +75,7 @@ resource "azurerm_app_service_plan" "testappplan" {
 }
 
 resource "azurerm_app_service" "testwebapp" {
-    name = "testwebapp568"
+    name = "testwebapp569"
     location = azurerm_resource_group.testrglabel.location
     resource_group_name = azurerm_resource_group.testrglabel.name
     app_service_plan_id = azurerm_app_service_plan.testappplan.id
